9969a9e6384e6748a30e651791a1d1e6d7e8ae5d
Balogun Tobi <mastertbal@gmail.com>
2022-11-08 15:15:27 +0100

    Revert "This is our third commit"
    This reverts commit b7d5c87e7933f7d160df6ccf3051f610106f2879.

--- app/src/main/java/com/eduteameight/gitforandroid/MainActivity.kt
+++ app/src/main/java/com/eduteameight/gitforandroid/MainActivity.kt
@@ -9,6 +9,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is contained in my second commit")
-        today is a good day
     }
 }
