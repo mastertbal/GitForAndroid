464167ba46fed8aaf97352924092511a438ca1be
Balogun Tobi <mastertbal@gmail.com>
2022-11-08 18:40:58 +0100

    Change 2 Commit

--- app/src/main/java/com/eduteameight/gitforandroid/MainActivity.kt
+++ app/src/main/java/com/eduteameight/gitforandroid/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is contained in my second commit")
-        println("Experimental")
+        println("CHANGE 2")
     }
 }
